801ea372c67646d852ec4335078a3262cad1ab6c
Raeein Bagheri <raeein@aol.com>
2023-12-02 20:31:28 -0500

    fix cell  spacing

--- WallHaven/HomeView.swift
+++ WallHaven/HomeView.swift
@@ -7,16 +7,31 @@
 
 import SwiftUI
 
-struct HomeView: View {
-    @State private var wallpapers = [Wallpaper]()
+struct ExecuteCode : View {
+    init( _ codeToExec: () -> () ) {
+        codeToExec()
+    }
     
-    @State private var cellSize = 100.0
+    var body: some View {
+        EmptyView()
+    }
+}
+
+struct HomeView: View {
+    @State private var wallpapers: Set<Wallpaper> = Set()
+    
+    @State private var cellWidth = 100.0
+    @State private var cellHeight = 100.0
     
     @State private var columns = [
-        GridItem(.fixed(cellSize), spacing: 1),
-        GridItem(.fixed(cellSize), spacing: 1),
-        GridItem(.fixed(cellSize), spacing: 1),
+        GridItem(.flexible(), spacing: 1),
+        GridItem(.flexible(), spacing: 1),
+        GridItem(.flexible(), spacing: 1),
     ]
+    
+    func test() {
+        print("Hi")
+    }
     
     var body: some View {
         
@@ -24,61 +39,94 @@ struct HomeView: View {
             GeometryReader { geo in
                 ScrollView {
                     LazyVGrid(columns: columns, spacing: 1) {
-                        ForEach(wallpapers, id: \.self) { wallpaper in
-                            AsyncImage(url: URL(string: wallpaper.path)) { phase in
+                        ForEach(wallpapers.sorted(by: { $0.id < $1.id }), id: \.self) { wallpaper in
                             
-                                switch phase {
-                                case .empty:
-                                    ProgressView()
-                                        .progressViewStyle(.circular)
-                                        .frame(width: 100, height: 200)
-                                case .failure:
-                                    Text(phase.error!.localizedDescription)
-                                case .success(let image):
-                                    image
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .scaledToFill()
-                                        .frame(width: 100, height: 200)
-                                        .clipped()
-                                        .cornerRadius(8)
-                                                                
-                                @unknown default:
-                                    fatalError()
+                            NavigationLink {
+                                ZStack {
+                                    Circle()
+                                        .fill(.red)
+                                    AsyncImage(url: URL(string: wallpaper.path))
+                                        .scaledToFit()
+                                    
+                                    Spacer()
+                                    HStack {
+                                        Text("Save")
+                                        Spacer()
+                                        Text("Download")
+                                    }
                                 }
                                 
+                            } label: {
+                                AsyncImage(url: URL(string: wallpaper.path)) { phase in
+                                    
+                                    switch phase {
+                                    case .empty:
+                                        ProgressView()
+                                            .progressViewStyle(.circular)
+                                            .frame(
+                                                width: geo.size.width / 3,
+                                                height: geo.size.height / 2.5
+                                            )
+                                            .background(.gray)
+                                            .cornerRadius(8)
+                                    case .failure:
+                                        ExecuteCode {
+                                            print(phase.error!.localizedDescription)
+                                            wallpapers.remove(wallpaper)
+                                        }
+                                    case .success(let image):
+                                        image
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fit)
+                                            .scaledToFill()
+                                            .frame(
+                                                width: geo.size.width / 3,
+                                                height: geo.size.height / 2.5
+                                            )                                        .clipped()
+                                            .cornerRadius(8)
+                                        
+                                    @unknown default:
+                                        fatalError()
+                                    }
+                                    
+                                }
                             }
-     
                         }
+                        
+                        
                     }
                 }
-            }
-            .navigationTitle("Home")
-            .toolbar(content: {
-                Button {
-                    print("Hi")
-                } label: {
-                    Label("filter", systemImage: "line.3.horizontal.decrease")
-                }
-                .font(.largeTitle)
-            })
-            .task(priority: .low) {
-                do {
-                    let url = URL(string: "https://wallhaven.cc/api/v1/search")!
-                    let (data, _) = try await URLSession.shared.data(from: url)
-                    let response = try JSONDecoder().decode(WallpaperResponse.self, from: data)
-                    
-                    for wallpaper in response.data {
-                        wallpapers.append(wallpaper)
+                .navigationTitle("Home")
+                .toolbar(content: {
+                    Button {
+                        print("Hi")
+                    } label: {
+                        Label("filter", systemImage: "line.3.horizontal.decrease")
                     }
-                } catch  {
-                    wallpapers = []
-                    print(error)
+                    .font(.largeTitle)
+                })
+                .task(priority: .low) {
+                    do {
+                        let url = URL(string: "https://wallhaven.cc/api/v1/search")!
+                        let (data, _) = try await URLSession.shared.data(from: url)
+                        let response = try JSONDecoder().decode(WallpaperResponse.self, from: data)
+                        
+                        for wallpaper in response.data {
+                            wallpapers.insert(wallpaper)
+                        }
+                    } catch  {
+                        wallpapers = []
+                        print(error)
+                    }
                 }
             }
         }
+        
+        
+
     }
 }
+
 
 #Preview {
     HomeView()
